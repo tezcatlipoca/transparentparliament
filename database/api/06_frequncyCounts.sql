ad18781efc9ee53c75d51642cc583696fcb438de
Andrew Sempere <a@feralresearch.org>
2020-02-23 21:59:06 +0100

    Correct frequencey count script to correctly namespaced year

--- database/api/06_frequncyCounts.sql
+++ database/api/06_frequncyCounts.sql
@@ -8,10 +8,10 @@ GRANT ALL PRIVILEGES ON api.wordcount TO web_anon;
 
 DROP VIEW IF EXISTS api.top_speaker_per_year;
 CREATE VIEW api.top_speaker_per_year AS
-SELECT DISTINCT ON (year) year,
+SELECT DISTINCT ON (date_year) date_year,
                           words_spoken,
                           speaker_name
 FROM api.top_speakers
-ORDER BY year, words_spoken DESC, speaker_name;
+ORDER BY date_year, words_spoken DESC, speaker_name;
 
 GRANT ALL PRIVILEGES ON api.top_speaker_per_year TO web_anon;
